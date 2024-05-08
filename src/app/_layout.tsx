42ad2bff1e82fb7dc6ffb2c990c82577e33a2691
tamimhasan197020 <tamimhasancu@gmail.com>
2024-05-09 02:35:50 +0600

    update package.json, _layout.tsx and tsconfig.json

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App() {
+	return (
+		<SafeAreaProvider>
+			<RootNavigation />
+
+			<StatusBar style="auto" />
+		</SafeAreaProvider>
+	)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const RootNavigation = () => {
+	return (
+		<Stack>
+			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+		</Stack>
+	)
+}
+
+export default App
